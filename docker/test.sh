eda255e6026ddce7f073b3c715696c1274f11a86
Reda Puenta <rida.hadj@hotmail.com>
2024-08-14 15:09:46 +0200

    fix: delete secret

--- docker/test.sh
+++
@@ -1,9 +0,0 @@
-
-sudo docker build -t test-mono:latest docker/  \
-  --build-arg GITHUB_ACCESS_TOKEN="a" \
-  --build-arg MONOREPO_TARGET_REPO="workspace-nest" \
-  --build-arg MONOREPO_TARGET_BRANCH="master" \
-  --build-arg APP_TARGET_REPO="api-control-test-with-mono" \
-  --build-arg APP_TARGET_BRANCH="master"
-
-sudo docker run test-mono 
